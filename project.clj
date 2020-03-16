c44a2a03033f89bcf9f39a35b4f7cd21acc08693
Jon Pither <jon.pither@gmail.com>
2020-03-16 08:59:01 +0000

    Initial port of Edge/Calcite demo

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
    "crux-kafka-embedded" "crux-kafka-connect" "crux-kafka"
    "crux-test"
    "crux-cli"
+   "crux-calcite"
    "crux-bench"])
 
 (defproject juxt/crux-dev "crux-dev-SNAPSHOT"
@@ -32,6 +33,7 @@
    [juxt/crux-metrics "derived-from-git"]
    [juxt/crux-http-server "derived-from-git" :exclusions [commons-codec]]
    [juxt/crux-rdf "derived-from-git"]
+   [juxt/crux-calcite "derived-from-git"]
    [juxt/crux-test "derived-from-git"]
    [juxt/crux-bench "derived-from-git" :exclusions [org.apache.commons/commons-lang3]]
    [juxt/crux-cli "derived-from-git"]
